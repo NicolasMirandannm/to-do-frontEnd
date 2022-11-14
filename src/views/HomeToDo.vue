eb6511d8a522f37e4005c901f0b965b07a77903a
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2022-11-14 18:33:30 -0400

    criação do corpo do projeto, e vinculação dos metodos no template

--- src/views/HomeToDo.vue
+++ src/views/HomeToDo.vue
@@ -5,73 +5,304 @@
         <div class="titulo">
           <h1>TO-DO APP</h1>
         </div>
-        <div class="listar-tarefas-por-lista"></div>
-        <div class="listar-todas-tarefas"></div>
+        <div class="listar-tarefas-por-lista">
+          <p class="label-list-tasks">Selecione a lista que deseja abrir:</p>
+          <b-form-select
+            class="dropdown"
+            v-model="listSelected"
+            :options="allLists"
+          ></b-form-select>
+          <button>Listar</button>
+        </div>
+        <div class="listar-todas-tarefas">
+          <p class="label-list-tasks">Listar todas tarefas cadastradas:</p>
+          <button>Listar</button>
+        </div>
         <div class="devs-nomes">
-          <h4>Nicolas Leonardo Miranda Lima</h4>
-          <h4>Eliel Lima de Aguiar</h4>
+          <h4 class="devName">devs:</h4>
+          <p class="devName">Nicolas Leonardo Miranda Lima</p>
+          <p class="devName">Eliel Lima de Aguiar</p>
         </div>
       </div>
       <div class="espaco-formulario">
-        <div class="add-tarefa"></div>
-        <div class="add-lista"></div>
-        <div class="ligar-tarefa-lista"></div>
+        <div class="add-tarefa">
+          <h3 class="titulo-forms">Adicionar uma nova tarefa</h3>
+          <div class="form-submit">
+            <p>tarefa:</p>
+            <input
+              type="text"
+              v-model="task.task"
+              placeholder="Digite sua tarefa..."
+            />
+            <button class="add-button" @click="createTask">
+              Adicionar Tarefa
+            </button>
+          </div>
+        </div>
+        <div class="add-lista">
+          <h3 class="titulo-forms">Adicionar uma nova lista</h3>
+          <div class="form-submit">
+            <p>Lista:</p>
+            <input
+              type="text"
+              required
+              v-model="list.name"
+              placeholder="Digite o nome da sua lista..."
+            />
+            <button class="add-button" @click="createList">
+              Adicionar Lista
+            </button>
+          </div>
+        </div>
+        <div class="ligar-tarefa-lista">
+          <h3 class="titulo-forms">Vincule uma tarefa com uma lista</h3>
+          <div class="link-dropdown">
+            <p>Selecione uma tarefa</p>
+            <b-form-select
+              class="dropdown"
+              v-model="TaskSelected"
+              :options="allTasks"
+            ></b-form-select>
+            <p>Selecione uma lista</p>
+            <b-form-select
+              class="dropdown"
+              v-model="listForLink"
+              :options="allLists"
+            ></b-form-select>
+          </div>
+          <button @click="linkTaskWithList">Vincular tarefa</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import api from '@/api-conection/axios-api-conection';
+import api from "@/api-conection/axios-api-conection";
 
 export default {
-  name: 'HomeToDo',
+  name: "HomeToDo",
   data() {
     return {
       task: {
-        task: '',
+        task: null,
         doneStatus: false,
       },
       list: {
-        name: '',
-        tasks: ''
+        name: null,
+        tasks: null,
       },
       allTasks: [],
       allLists: [],
-    }
+      listSelected: null,
+      TaskSelected: null,
+      listForLink: null
+    };
   },
   mounted() {
-    api.get('task/findAll').then((response) => {
-      this.allTasks = response.data;
+    api.get("task/findAll").then((response) => {
+      const tasks = response.data;
+      this.allTasks = tasks.map((task) => {
+        return {
+          value: task.task,
+          text: task.task,
+        };
+      });
     }),
-    api.get('list/findAll').then((response) => {
-      this.allLists = response.data
-    })
+      api.get("list/findAll").then((response) => {
+        const lists = response.data;
+        this.allLists = lists.map((list) => {
+          return {
+            value: list.name,
+            text: list.name,
+          };
+        });
+      });
   },
   methods: {
     createTask() {
       const taskReq = this.task;
-      api.post("task/create", taskReq).then((response) => {
-        console.log(response)
-      })
-      this.task.task = '';
-      this.task.doneStatus = false;
+      if (taskReq.task == null) {
+        alert("Digite sua tarefa!!");
+      } else {
+        try {
+          api.post("task/create", taskReq).then((response) => {
+            alert(`tarefa '${response.data.task}' criada com sucesso!`);
+          });
+          window.location.reload();
+        } catch (err) {
+          alert(err.message);
+        }
+      }
     },
     createList() {
-      const listReq = this.list
-      api.post("list/create", listReq).then((response) => {
-        console.log(response);
-      })
-      this.list.name = '',
-      this.list.tasks = []
+      const listReq = this.list;
+      if (listReq.name == null) {
+        alert("Digite o nome da lista!!");
+      } else {
+        try {
+          api.post("list/create", listReq).then((response) => {
+            alert(`lista '${response.data.name}' criada com sucesso!`);
+            window.location.reload();
+          });
+        } catch (err) {
+          alert(err.message);
+        }
+      }
     },
-    linkTaskWithList(list, task) {
-      const linkRequest = {task, list};
-      api.put('add-task-in-list', linkRequest).then((response) => {
-        console.log(response);
-      })
-    }
-  }
-  
-}
+    linkTaskWithList() {
+      const linkRequest = {
+        task: {
+          task: this.TaskSelected,
+        },
+        list: {
+          name: this.listForLink,
+        },
+      };
+      if (linkRequest.task.task == null || linkRequest.list.name == null) {
+        alert("ambos os campos são necessarios!!");
+      } else {
+        try {
+          api.put("add-task-in-list", linkRequest).then((response) => {
+            alert(
+              `${this.TaskSelected} adicionado com sucesso em ${this.listSelected}`
+            );
+            window.location.reload();
+          });
+        } catch (err) {
+          alert(err.message);
+        }
+      }
+    },
+  },
+};
 </script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.home {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: #343a40;
+}
+
+.container {
+  height: 80%;
+  width: 80%;
+  background-color: #e0ddcf;
+  border-radius: 5px;
+  box-shadow: 1px 1px 2px 3px #212529;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+}
+.menu-lateral {
+  height: 100%;
+  width: 30%;
+  background-color: #534b52;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  align-items: center;
+  border-top-right-radius: 0%;
+  border-bottom-right-radius: 0%;
+}
+
+.label-list-tasks {
+  color: #f1f0ea;
+  font-weight: bolder;
+}
+
+.devs-nomes {
+  background-color: #2d232e;
+  width: 80%;
+  height: 25%;
+  margin-bottom: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #f1f0ea;
+  font-size: 15px;
+  border-radius: 5px;
+}
+
+.devName {
+  margin-top: 15px;
+}
+
+.titulo {
+  height: 25%;
+  width: 80%;
+  background-color: #2d232e;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  color: #f1f0ea;
+}
+
+.listar-tarefas-por-lista {
+  width: 80%;
+  height: 15%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.listar-todas-tarefas {
+  width: 80%;
+  height: 15%;
+  margin-bottom: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.espaco-formulario {
+  background-color: #e0ddcf;
+  position: relative;
+  height: 100%;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 5px;
+  border-top-left-radius: 0%;
+  border-bottom-left-radius: 0%;
+}
+
+.add-tarefa {
+  width: 80%;
+  height: 25%;
+  border-radius: 5px;
+  background-color: #cac7bf;
+}
+
+.add-lista {
+  width: 80%;
+  height: 25%;
+  border-radius: 5px;
+  background-color: #cac7bf;
+}
+
+.ligar-tarefa-lista {
+  width: 80%;
+  height: 25%;
+  border-radius: 5px;
+  background-color: #cac7bf;
+}
+</style>
--- src/views/ListAllTasks.vue
+++ src/views/ListAllTasks.vue
@@ -1,17 +1,43 @@
 <template>
-    <div>
+    <div class="main">
         <div class="container">
             <div class="barra-lateral">
                 <div class="titulo">
                   <h1>TO-DO APP</h1>
                 </div>
                 <div class="titulo-da-rota">
-                    <h1>TODAS TAREFAS CRIADAS</h1>
+                    <p>TODAS TAREFAS CRIADAS</p>
                 </div>
             </div>
             <div class="container-das-tarefas">
                 <div class="barra-de-pesquisa">BARRA DE PESQUISA</div>
-                <div class="listas-tarefas">V-FOR COM AS TAREFAS</div>
+                <div class="listas-tarefas">
+                    <b-list-group-item class="lista-titulo">
+                        <p>TAREFA</p>
+                        <p>STATUS</p>
+                        <p>LISTA</p>
+                        <p>AÇÕES</p>
+                    </b-list-group-item>
+                    <b-list-group class="list-group" v-for="task in allTasks" :key="task.id">
+                        <b-list-group-item class="items">
+                            <div class="input-task">
+                                <input type="text" v-model="task.task">
+                                <button>Editar</button>
+                            </div>
+                            <div class="task-status">
+                                <p>{{task.doneStatus}}</p>
+                                <button>Concluir task</button>
+                            </div>
+                            <div class="lista-name">
+                                <p v-if="task.list != null">{{task.list.name}}</p>
+                            </div>
+                            <div class="acoes">
+                                <button>deletar</button>
+                                <button>remover da lista</button>
+                            </div>
+                        </b-list-group-item>
+                    </b-list-group>
+                </div>
             </div>
         </div>
     </div>
@@ -24,13 +50,141 @@ export default {
     name: 'ListAllTasks',
     data() {
         return {
-            tasks: []
+            allTasks: [ {
+                id: null,
+                list: {
+                    name: null
+                },
+                doneStatus: null,
+                task: null
+            }]
         }
     },
-    created() {
-        api.get('task/findAll').then((response) => {
-            this.tasks = response.data;
-        })       
+    mounted() {
+        try {
+            api.get('task/findAll').then((response) => {
+                this.allTasks = response.data;
+                console.log(this.tasks)
+                console.log(response.data)
+            })       
+        } catch(err) {
+            alert(err.message)
+        }
     }
 }
 </script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: #343a40;
+}
+.container {
+    width: 80%;
+    height: 80%;
+    background-color: #ced4da;
+    box-shadow: 1px 2px 2px 3px #212529;
+    display: flex;
+    position: absolute;
+    flex-direction: row;
+    padding: 0;
+}
+
+.barra-lateral {
+    width: 30%;
+    background-color: #adb5bd;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+}
+.titulo {
+  height: 30%;
+  width: 80%;
+  background-color: #495057;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  color: #f1f0ea;
+}
+
+.titulo-da-rota {
+    height: 30%;
+    width: 80%;
+    background-color: #ced4da;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10%;
+    margin-bottom: 10%;
+    color: #495057;
+    font-weight: bold;
+    font-size: 20px;
+}
+
+.container-das-tarefas {
+    background-color: #f8f9fa;
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.barra-de-pesquisa {
+    height: 15%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.listas-tarefas {
+    background-color: #ced4da;
+    width: 90%;
+    height: 80%;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+
+
+.lista-titulo {
+    margin-top: 10px;
+    width: 95%;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    font-weight: 600;
+    color: #495057;
+}
+
+.list-group{
+    width: 95%;
+}
+
+.items {
+    width: 100%;
+    display: flex;    
+}
+</style>
