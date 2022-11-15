be3d7deff919a52f5c31d6a9c88e3a6fdd139d2d
Nicolas Leonardo Miranda Lima <nicolas.lima@in.cassems.com.br>
2022-11-15 07:53:53 -0400

    implementação das ações dos botões

--- src/views/ListAllTasks.vue
+++ src/views/ListAllTasks.vue
@@ -1,112 +1,191 @@
 <template>
-    <div class="main">
-        <div class="container">
-            <div class="barra-lateral">
-                <div class="titulo">
-                  <h1>TO-DO APP</h1>
-                </div>
-                <div class="titulo-da-rota">
-                    <p>TODAS TAREFAS CRIADAS</p>
-                </div>
-            </div>
-            <div class="container-das-tarefas">
-                <div class="barra-de-pesquisa">BARRA DE PESQUISA</div>
-                <div class="listas-tarefas">
-                    <b-list-group-item class="lista-titulo">
-                        <p>TAREFA</p>
-                        <p>STATUS</p>
-                        <p>LISTA</p>
-                        <p>AÇÕES</p>
-                    </b-list-group-item>
-                    <b-list-group class="list-group" v-for="task in allTasks" :key="task.id">
-                        <b-list-group-item class="items">
-                            <div class="input-task">
-                                <input type="text" v-model="task.task">
-                                <button>Editar</button>
-                            </div>
-                            <div class="task-status">
-                                <p>{{task.doneStatus}}</p>
-                                <button>Concluir task</button>
-                            </div>
-                            <div class="lista-name">
-                                <p v-if="task.list != null">{{task.list.name}}</p>
-                            </div>
-                            <div class="acoes">
-                                <button>deletar</button>
-                                <button>remover da lista</button>
-                            </div>
-                        </b-list-group-item>
-                    </b-list-group>
-                </div>
-            </div>
+  <div class="main">
+    <div class="container">
+      <div class="barra-lateral">
+        <div class="titulo">
+          <h1>TO-DO APP</h1>
         </div>
+        <div class="titulo-da-rota">
+          <p>TODAS TAREFAS CRIADAS</p>
+        </div>
+      </div>
+      <div class="container-das-tarefas">
+        <div class="barra-de-pesquisa">
+          <div class="filtro-lista">
+            <b-form-select
+              class="dropdown"
+              v-model="listSelected"
+              :options="allLists"
+            ></b-form-select>
+            <button>Filtrar</button>
+            <button>Esvaziar lista</button>
+          </div>
+          <div class="busca">
+            <p>Buscar</p>
+            <input type="text" placeholder="Nome da tarefa...">
+          </div>
+        </div>
+        <div class="listas-tarefas">
+          <b-list-group-item class="lista-titulo">
+            <p>TAREFA</p>
+            <p>STATUS</p>
+            <p>LISTA</p>
+            <p>AÇÕES</p>
+          </b-list-group-item>
+          <b-list-group
+            class="list-group"
+            v-for="task in allTasks"
+            :key="task.id"
+          >
+            <b-list-group-item class="items">
+              <div class="input-task">
+                <input type="text" v-model="task.task" />
+                <button @click="updateTask(task)">Editar</button>
+              </div>
+              <div class="task-status">
+                <p>{{ task.doneStatus }}</p>
+                <button @click="doneTask(task)">Concluir task</button>
+              </div>
+              <div class="lista-name">
+                <p v-if="task.list != null">{{ task.list.name }}</p>
+                <p v-else>Sem lista</p>
+              </div>
+              <div class="acoes">
+                <button @click="deleteTask(task)">deletar</button>
+                <button @click="removerDaLista(task)">remover da lista</button>
+              </div>
+            </b-list-group-item>
+          </b-list-group>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import api from '@/api-conection/axios-api-conection';
+import api from "@/api-conection/axios-api-conection";
 
 export default {
-    name: 'ListAllTasks',
-    data() {
-        return {
-            allTasks: [ {
-                id: null,
-                list: {
-                    name: null
-                },
-                doneStatus: null,
-                task: null
-            }]
-        }
-    },
-    mounted() {
+  name: "ListAllTasks",
+  data() {
+    return {
+      listSelected: null,
+      allTasks: [
+        {
+          id: null,
+          list: {
+            name: null,
+          },
+          doneStatus: null,
+          task: null,
+        },
+      ],
+      allLists: []
+    };
+  },
+  mounted() {
+    try {
+      api.get("task/findAll").then((response) => {
+        this.allTasks = response.data;
+        console.log(this.tasks);
+        console.log(response.data);
+      });
+    } catch (err) {
+      alert(err.message);
+    };
+    
+    try {
+        api.get("list/findAll").then((response) => {
+            const lists = response.data;
+            this.allLists = lists.map((list) => {
+            return {
+                value: list.name,
+                text: list.name,
+            };
+            });
+        });
+    } catch(err) {
+        alert(err.message)
+    }
+  },
+  methods: {
+    updateTask(task) {
         try {
-            api.get('task/findAll').then((response) => {
-                this.allTasks = response.data;
-                console.log(this.tasks)
-                console.log(response.data)
-            })       
+            api.put('task/update', task).then((response) => {
+                alert(`${response.data.task} alterado com sucesso`);
+            })
+            
+
         } catch(err) {
             alert(err.message)
         }
+    },
+    doneTask(task) {
+
+        let taskDone = task;
+        taskDone.doneStatus = true;
+        try {
+            api.put('task/update', taskDone).then((response) => {
+                alert(`Parabens por concluir a tarefa: ${response.data.task}`)
+            })
+        } catch(err) {
+            alert(err.message)
+        }
+    },
+    deleteTask(task) {
+        try {
+            api.delete('task/delete', {data: task}).then((response) => {
+                alert(`Tarefa ${response.data.task} deletada com sucesso`)
+            })
+            window.location.reload();
+        } catch(err) {
+            alert(err.message)
+        }
+    },
+    removerDaLista(task) {
+        api.put('remove-task-from-list', task).then((response) => {
+            alert(`tarefa ${response.data.task} removido com sucesso da lista '${task.list.name}'`)
+        }).catch((err) => {
+            alert('Tarefa não pertence a nenhuma lista')
+        })
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 * {
-    margin: 0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
 }
 
 .main {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background-color: #343a40;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #343a40;
 }
 .container {
-    width: 80%;
-    height: 80%;
-    background-color: #ced4da;
-    box-shadow: 1px 2px 2px 3px #212529;
-    display: flex;
-    position: absolute;
-    flex-direction: row;
-    padding: 0;
+  width: 80%;
+  height: 80%;
+  background-color: #ced4da;
+  box-shadow: 1px 2px 2px 3px #212529;
+  display: flex;
+  position: absolute;
+  flex-direction: row;
+  padding: 0;
 }
 
 .barra-lateral {
-    width: 30%;
-    background-color: #adb5bd;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
+  width: 30%;
+  background-color: #adb5bd;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 }
 .titulo {
   height: 30%;
@@ -123,68 +202,71 @@ export default {
 }
 
 .titulo-da-rota {
-    height: 30%;
-    width: 80%;
-    background-color: #ced4da;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    margin-top: 10%;
-    margin-bottom: 10%;
-    color: #495057;
-    font-weight: bold;
-    font-size: 20px;
+  height: 30%;
+  width: 80%;
+  background-color: #ced4da;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  color: #495057;
+  font-weight: bold;
+  font-size: 20px;
 }
 
 .container-das-tarefas {
-    background-color: #f8f9fa;
-    width: 70%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  background-color: #f8f9fa;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .barra-de-pesquisa {
-    height: 15%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  height: 15%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 .listas-tarefas {
-    background-color: #ced4da;
-    width: 90%;
-    height: 80%;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  background-color: #ced4da;
+  width: 90%;
+  height: 80%;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
 }
-
-
 
 .lista-titulo {
-    margin-top: 10px;
-    width: 95%;
-    height: 50px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom-left-radius: 5px;
-    border-bottom-right-radius: 5px;
-    font-weight: 600;
-    color: #495057;
+  margin-top: 10px;
+  width: 95%;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  font-weight: 600;
+  color: #495057;
 }
 
-.list-group{
-    width: 95%;
+.list-group {
+  width: 95%;
 }
 
 .items {
-    width: 100%;
-    display: flex;    
+  width: 100%;
+  height: 80px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
